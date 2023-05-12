9999f595ca32e3eceaadec2be3f6b1bdc41f99c0
wnddms12345 <wnddms12345@naver.com>
2023-05-12 23:11:20 +0900

    doing now

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,50 @@
-import Button from "./Button";
-import styles from "./App.module.css"
-function App() {
-  const [todo,setTodo] = useState("");
-  return (
 
+import {useState, useEffect} from "react";
+function App() {
+  const [loading,setLoading] = useState(true);
+  const [coins,setCoins] = useState([]);
+  const [userDollar, setUserDollar] = useState(-1);
+
+  const submitDollar = (event)=>{
+    event.preventDefault();
+  // if (event.target.value < 0)
+  //   return;
+    setUserDollar(event.target.value);
+  };
+  useEffect (()=>{
+    fetch ("https://api.coinpaprika.com/v1/tickers")
+    .then((response)=>response.json())
+    .then((json)=>{
+      setCoins(json);
+      setLoading(false);
+    });
+  },[]);
+  useEffect( setUserDollar
+  ,[userDollar]);
+
+  return (
     <div>
-      <input type="text" placeholder="Write your to do...."/>
+      <h1>The Coins!{loading ? "" : `(${coins.length})`}</h1>
+      <div>
+        <form onSubmit={submitDollar}>
+        <label htmlFor="dollar"><strong>input your dollar :</strong></label>
+        <input
+          id = "dollar"
+          placeholder='input your USD'
+          type="number"
+        />
+         <button>See in coin</button>
+        </form>
+      </div>
+      <h3>{userDollar < 0 ? "" :`Your Money is ${userDollar} $` }</h3>
+      {loading ? <strong>Loading....</strong> : <select>
+        {coins.map((coin)=>(
+        <option key={coin.rank}>
+          {coin.name} ({coin.symbol}) : {coin.quotes.USD.price}
+        </option>
+        ))}
+      </select>}
+
     </div>
   );
 }
