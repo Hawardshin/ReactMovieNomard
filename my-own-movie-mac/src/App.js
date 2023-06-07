1efaa554ddd6d024ed385b9c16e2abb25094881b
wnddms12345 <wnddms12345@naver.com>
2023-06-07 22:43:06 +0900

    this is react movie app prototpye in mac

--- my-own-movie-mac/src/App.js
+++ my-own-movie-mac/src/App.js
@@ -1,35 +1,23 @@
-import {useState, useEffect} from "react";
-function App() {
-  const [todo,setTodo] = useState("");
-  const [toDos,setToDos] = useState([]);
-  const onChange = (event)=>setTodo(event.target.value);
-  // console.log(todo);
-  const onSubmit = (event)=> {
-    event.preventDefault(); //랜더링이 다시 되는 것을 방지.
-    if (todo == "")
-      return;
-          setToDos(currentArray=>[todo,...currentArray]);
-      setTodo("");
-  };
-  console.log(toDos);
+import React from "react";
+import Home from "./routes/Home"
+import Detail from './routes/Detail';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+function App(){
   return (
-    <div>
-      <h1>My To Dos({toDos.length})</h1>
-      <form onSubmit={onSubmit}>
-        <input
-          onChange = {onChange}
-          value = {todo}
-          type="text"
-          placeholder="Write your to do...."
-        />
-        <button>Add To Do</button>
-      </form>
-      <hr />
-      <ul>
-        {toDos.map((itm,index)=> <li key={index}>{itm}</li>)}
-      </ul>
-    </div>
-  );
+  <Router>
+    <Switch>
+      <Route path="/movie/:id">
+        <Detail/>
+      </Route>
+      <Route path="/">
+        <Home/>
+      </Route>
+    </Switch>
+  </Router>
+  )
 }
-
 export default App;
