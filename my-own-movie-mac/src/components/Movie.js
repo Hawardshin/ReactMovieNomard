c7737c25fe7ad0578b032a0d813908f1e7f3645e
wnddms12345 <wnddms12345@naver.com>
2023-06-08 21:57:42 +0900

    aaa

--- my-own-movie-mac/src/components/Movie.js
+++ my-own-movie-mac/src/components/Movie.js
@@ -2,25 +2,44 @@ import PropTypes from "prop-types";
 import {Link} from "react-router-dom";
 import altImage from "../assets/noMoive.png"
 import style from "./Movie.module.css"
+import { useState } from "react";
 const handleImageError = (event) => {
   event.target.src = altImage;
   event.target.style = "width:40"
 };
 
-
 function Movie({id,coverImage,title,summary,genres}){
-  console.log(coverImage);
+  const [isHovered, setIsHovered] = useState(false);
+  const handleMouseEnter = () => {
+    setIsHovered(true);
+  };
+  const handleMouseLeave = () => {
+    setIsHovered(false);
+  };
+
   return (
-  <div className = {style.pic}>
+  <div
+    className = {style.pic}
+    onMouseEnter={handleMouseEnter}
+    onMouseLeave={handleMouseLeave}
+  >
     <h2>
       <Link to={`/movie/${id}`}>
-        <img src={coverImage} onError={handleImageError}
+      <a>
+      <div className = {style.screen}>
+        <div
+          className={`${style.top} ${isHovered ? style.hovered : ""}`}
+        >하하gjkgjgjgjgjgjsssssㅁㅁㅁㅁㅁj</div>
+      </div>
+      <img src={coverImage} onError={handleImageError}
         alt= ""
       />
+      </a>
       </Link>
     </h2>
     <ul className={style.gallery}>
     <a className={style.title}>{title}</a>
+
     {genres.map((genre, index) => {
       if (index % 2 === 0) {
         const nextGenre = genres[index + 1]; // 다음 genre 가져오기
