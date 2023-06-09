240bf96789e34e774719ede2eca29f2703637a9c
Hawardshin <wnddms12345@naver.com>
2023-06-09 15:29:35 +0900

    to much module

---
+++ bitcoin-window/src/App.js
@@ -0,0 +1,76 @@
+// import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+// import Detail from "./routes/Detail";
+// import Home from "./routes/Home";
+
+// function App() {
+//   return(<Router>
+//     <Switch>
+//       <Route path="/hello">
+//         <h1>Hello</h1>
+//       </Route>
+//       <Route path="/movie/:id">
+//         <Detail />
+//         </Route>
+//         <Route path="/">
+//         <Home />
+//       </Route>
+//     </Switch>
+//   </Router>);
+// }
+
+// export default App;
+
+
+
+import {useState, useEffect} from "react";
+function App() {
+  const [loading,setLoading] = useState(true);
+  const [coins,setCoins] = useState([]);
+  const [userDollar, setUserDollar] = useState(0);
+
+  const onChange = (event)=>{
+    event.preventDefault();
+    setUserDollar(event.target.value);
+  };
+  useEffect (()=>{
+    fetch ("https://api.coinpaprika.com/v1/tickers")
+    .then((response)=>response.json())
+    .then((json)=>{
+      setCoins(json);
+      setLoading(false);
+    });
+  },[]);
+  return (
+    <div>
+      <h1>The Coins! {loading ? "" : `(number of coins : ${coins.length})`}</h1>
+      <div>
+        <label htmlFor="dollar"><strong>input your dollar :</strong></label>
+        <input
+          id = "dollar"
+          placeholder='input your USD'
+          type="number"
+          value = {userDollar}
+          onChange={onChange}
+        />
+      </div>
+      <h3>{userDollar <= 0 ? "Each coin per dollar" :`The value of your ${userDollar}$ in coins` }</h3>
+      {loading ? <strong>Loading....</strong> :
+        userDollar <= 0 ?  <select>
+        {coins.map((coin)=>(
+        <option key={coin.rank}>
+          {coin.name} ({coin.symbol}) : {coin.quotes.USD.price}
+        </option>
+        ))}
+      </select>
+        :<select>
+        {coins.map((coin)=>(
+        <option key={coin.rank}>
+          {coin.name} ({coin.symbol}) : {userDollar *  1 /coin.quotes.USD.price}
+        </option>
+        ))}
+      </select>}
+    </div>
+  );
+}
+
+export default App;
